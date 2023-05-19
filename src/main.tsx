944b321a8553c9eeeb745dd2f01af3a158a63d7a
MD Nahasat Hossain Nibir <nahasat.nibir@gmail.com>
2023-05-19 22:12:22 +0600

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import Router from './routes';
+import Router from "./routes";
 
-import '@styles/global.scss';
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import "@styles/global.scss";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Router />
-  </React.StrictMode>,
+    <BrowserRouter basename="https://wosamfrontend-production.up.railway.app">
+      <Router />
+    </BrowserRouter>
+  </React.StrictMode>
 );
