4f2d518341582f335bf086e9f4fd430fa9076c4b
MD Nahasat Hossain Nibir <nahasat.nibir@gmail.com>
2023-05-19 15:46:34 +0600

    Delete src directory

--- src/routes/dashboard/user/route.tsx
+++
@@ -1,9 +0,0 @@
-import type { RouteObject } from 'react-router-dom';
-
-import UserPage from './page';
-
-const userRoute:RouteObject = {
-  path: '/dashboard/user',
-  element: <UserPage />,
-};
-export default userRoute;
--- src/types/interaction.ts
+++
@@ -1,3 +0,0 @@
-import type { PressEvent } from '@react-types/shared';
-
-export type PressHandler = (evt:PressEvent) => void;
